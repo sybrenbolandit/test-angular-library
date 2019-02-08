d4642a2120422aca2d0a2df606f8a02a5a5d4f33
sybrenbolandit <sybrenbolandit@gmail.com>
2019-02-08 18:09:08 +0100

    Initial commit angular library

---
+++ projects/demo/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LibModule } from '../../../lib/src/lib/lib.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    LibModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
